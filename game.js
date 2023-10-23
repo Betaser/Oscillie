468e51bb54b45fb0d42a622dae65f18f36ad3b55
Betaser <tylerharrisonhsieh@gmail.com>
2023-10-23 03:38:35 -0400

    messing with hot reload

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
+
 // Beginning of getting shooting input.
 document.addEventListener("click", (e) => {
+    let tips = document.getElementsByClassName("tips")[0];
+    tips.querySelector("p").innerHTML += "!";
     console.log(e);
 });
