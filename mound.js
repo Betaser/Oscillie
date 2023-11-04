43d34c1602f614c2d52f638dcd231fe1a4d8c1bb
Betaser <tylerharrisonhsieh@gmail.com>
2023-11-03 20:38:45 -0400

    Added mound.js, was needed to get code to work normally

---
+++ mound.js
@@ -0,0 +1,30 @@
+class Mound {
+
+    static MakeDiv(position, size, units="v") {
+        const element = document.createElement("div");
+        element.className = "mound";
+        renderElementSize(element, size, units);
+        renderElement(element, position, units);
+        document.body.appendChild(element);
+
+        const mound = new Mound(element, position);
+        return mound;
+    }
+
+    constructor(element, position) {
+        this.element = element;
+        this.position = position;
+        this.bounds = Polygon.fromBoundingRect(this.element.getBoundingClientRect());
+    }
+
+    update() {}
+
+    render() {
+        renderElement(this.element, this.position, "v");
+    }
+
+    renderBounds() {
+        renderElementBounds(this.bounds);
+    }
+
+}
